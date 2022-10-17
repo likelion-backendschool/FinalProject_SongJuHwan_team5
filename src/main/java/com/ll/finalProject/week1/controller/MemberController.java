3f70f07bce0a3fc299c08775c1cf9f89a2db3182
jootang2 <joohwan.song15b@gmail.com>
2022-10-17 23:46:47 +0900

    :sparkles: feat: Member DB 생성

---
+++ src/main/java/com/ll/finalProject/week1/controller/MemberController.java
@@ -0,0 +1,4 @@
+package com.ll.finalProject.week1.controller;
+
+public class MemberController {
+}
---
+++ src/main/java/com/ll/finalProject/week1/domain/Member.java
@@ -0,0 +1,33 @@
+package com.ll.finalProject.week1.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
+
+    @Column(unique = true, name = "USER_NAME")
+    private String userName;
+
+    @Column( name = "USER_PASSWORD")
+    private String password;
+
+    @Column(unique = true, name = "USER_NICKNAME")
+    private String nickName;
+
+    @Column(name = "USER_EMAIL")
+    private String eMail;
+
+    @Column( name = "USER_AUTH")
+    private Integer authLevel;
+
+}
