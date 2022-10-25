31f5819f6bd9c98448717e46867b567bdf1b5a7e
jootang2 <joohwan.song15b@gmail.com>
2022-10-25 13:24:36 +0900

    :sparkles: feat : 토스 결제 백엔드 연동

--- 2Week_Mission/src/main/java/com/ll/finalProject/week2/controller/OrderController.java
+++ 2Week_Mission/src/main/java/com/ll/finalProject/week2/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.ll.finalProject.week2.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.finalProject.week2.domain.CartItem;
 import com.ll.finalProject.week2.domain.Member;
 import com.ll.finalProject.week2.domain.OrderItem;
@@ -9,24 +11,31 @@ import com.ll.finalProject.week2.service.MemberService;
 import com.ll.finalProject.week2.service.OrderItemService;
 import com.ll.finalProject.week2.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/order")
 public class OrderController {
 
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final ObjectMapper objectMapper;
     private final MemberService memberService;
     private final OrderService orderService;
     private final OrderItemService orderItemService;
@@ -60,6 +69,60 @@ public class OrderController {
         model.addAttribute("order", ordered);
         model.addAttribute("orderItemList", orderItemList);
         return "order/detail";
+    }
 
+    @PostConstruct
+    private void init() {
+        restTemplate.setErrorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse response) {
+                return false;
+            }
+
+            @Override
+            public void handleError(ClientHttpResponse response) {
+            }
+        });
+    }
+
+    private final String SECRET_KEY = "test_sk_D4yKeq5bgrp1N6OD5pQ8GX0lzW6Y";
+
+    @RequestMapping("/{id}/success")
+    public String confirmPayment(
+            @RequestParam String paymentKey, @RequestParam String orderId, @RequestParam Long amount,
+            Model model) throws Exception {
+
+        HttpHeaders headers = new HttpHeaders();
+        // headers.setBasicAuth(SECRET_KEY, ""); // spring framework 5.2 이상 버전에서 지원
+        headers.set("Authorization", "Basic " + Base64.getEncoder().encodeToString((SECRET_KEY + ":").getBytes()));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, String> payloadMap = new HashMap<>();
+        payloadMap.put("orderId", orderId);
+        payloadMap.put("amount", String.valueOf(amount));
+
+        HttpEntity<String> request = new HttpEntity<>(objectMapper.writeValueAsString(payloadMap), headers);
+
+        ResponseEntity<JsonNode> responseEntity = restTemplate.postForEntity(
+                "https://api.tosspayments.com/v1/payments/" + paymentKey, request, JsonNode.class);
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            JsonNode successNode = responseEntity.getBody();
+            model.addAttribute("orderId", successNode.get("orderId").asText());
+            String secret = successNode.get("secret").asText(); // 가상계좌의 경우 입금 callback 검증을 위해서 secret을 저장하기를 권장함
+            return "order/success";
+        } else {
+            JsonNode failNode = responseEntity.getBody();
+            model.addAttribute("message", failNode.get("message").asText());
+            model.addAttribute("code", failNode.get("code").asText());
+            return "order/fail";
+        }
+    }
+
+    @RequestMapping("/{id}/fail")
+    public String failPayment(@RequestParam String message, @RequestParam String code, Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("code", code);
+        return "order/fail";
     }
 }
